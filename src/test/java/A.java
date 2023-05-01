76c0cac44c21cb9204a884fd2677238df50b28cf
KokilaSuresh03 <kokila0392@gmail.com>
2023-05-01 18:25:02 +0530

    1st Push

---
+++ src/test/java/A.java
@@ -0,0 +1,16 @@
+
+public class A {
+	private void method1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method3() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
